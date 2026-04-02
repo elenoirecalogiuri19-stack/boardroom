a8d2a49d28ecb8ab7e0b56b750f0d7c477b8654d
elenoirecalogiuri19-stack <elenoirecalogiuri19@gmail.com>
2026-04-02 15:35:40 +0200

    Storico completo e funzionante

--- src/main/webapp/app/storico-prenotazioni/storico-prenotazioni.component.ts
+++ src/main/webapp/app/storico-prenotazioni/storico-prenotazioni.component.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import SharedModule from 'app/shared/shared.module';
 import { IPrenotazioni } from 'app/entities/prenotazioni/prenotazioni.model';
 import { PrenotazioniService } from 'app/entities/prenotazioni/service/prenotazioni.service';
-import { faCalendar, faCalendarDays, faCalendarWeek } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar, faCalendarDays, faCalendarWeek, faUser, faTableColumns } from '@fortawesome/free-solid-svg-icons';
 import dayjs from 'dayjs/esm';
 import 'dayjs/esm/locale/it';
 
@@ -23,7 +23,9 @@ export class StoricoPrenotazioniComponent implements OnInit {
   tutteLePrenotazioni: IPrenotazioni[] = [];
   filtroAttivo = signal<FiltroModalita>('tutte');
   isLoading = signal<boolean>(false);
+
   readonly annoCorrente = dayjs().year();
+  readonly ieri = dayjs().subtract(1, 'day').format('YYYY-MM-DD');
 
   filtroDataSingola = '';
   filtroDataDal = '';
@@ -32,6 +34,8 @@ export class StoricoPrenotazioniComponent implements OnInit {
   faCalendar = faCalendar;
   faCalendarDays = faCalendarDays;
   faCalendarRange = faCalendarWeek;
+  faUser = faUser;
+  faTableColumns = faTableColumns;
 
   private prenotazioniService = inject(PrenotazioniService);
 
@@ -58,7 +62,11 @@ export class StoricoPrenotazioniComponent implements OnInit {
     this.prenotazioniService.getStorico().subscribe({
       next: res => {
         const dati = res.body ?? [];
-        this.tutteLePrenotazioni = dati.sort((a, b) => dayjs(b.data).valueOf() - dayjs(a.data).valueOf());
+
+        const ieridayjs = dayjs().subtract(1, 'day').endOf('day');
+        this.tutteLePrenotazioni = dati
+          .filter(p => (p.data && dayjs(p.data).isBefore(ieridayjs)) || dayjs(p.data).isSame(ieridayjs, 'day'))
+          .sort((a, b) => dayjs(b.data).valueOf() - dayjs(a.data).valueOf());
         this.applicaFiltroCorrente();
         this.isLoading.set(false);
       },
@@ -89,13 +97,16 @@ export class StoricoPrenotazioniComponent implements OnInit {
   }
 
   private applicaFiltroCorrente(): void {
-    const oggi = dayjs();
     const tipo = this.filtroAttivo();
+
     if (tipo === 'tutte') {
       this.prenotazioniMostrate.set(this.tutteLePrenotazioni);
     } else if (tipo === '30giorni') {
-      const limite = oggi.subtract(30, 'days');
-      this.prenotazioniMostrate.set(this.tutteLePrenotazioni.filter(p => p.data && dayjs(p.data).isAfter(limite)));
+      const dal = dayjs().subtract(30, 'days').startOf('day');
+      const al = dayjs().subtract(1, 'day').endOf('day');
+      this.prenotazioniMostrate.set(
+        this.tutteLePrenotazioni.filter(p => p.data && !dayjs(p.data).isBefore(dal) && !dayjs(p.data).isAfter(al)),
+      );
     } else if (tipo === 'annoCorrente') {
       this.prenotazioniMostrate.set(this.tutteLePrenotazioni.filter(p => p.data && dayjs(p.data).year() === this.annoCorrente));
     } else if (tipo === 'singola' && this.filtroDataSingola) {
